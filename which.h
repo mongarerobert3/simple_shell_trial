80b828d4a795b0a05117ca35a46cc93fb9f5f56d
mongarerobert3 <mongarerobert3@gmail.com>
2022-05-18 13:47:03 +0300

    simple shell

--- which.h
+++ which.h
@@ -1,5 +1,5 @@
-#ifndef  FILES_H
-#define FILES_H
+#ifndef  WHICH_H
+#define WHICH_H
 
 /**
  * main - main program files
@@ -10,22 +10,43 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd,h>
+#include <fcntl.h>           /* Definition of AT_* constants */
+#include <sys/stat.h>
 
 extern char **environ;
-/***dummy typedef ***/
-typedef int make_iso_compilers_happy;
 
 /*********** String functions *************/
 
 char *_strcpy(char *dest, const char *src);
 unsigned int _strlen(const char *str);
-int _strcmp(char *s1, char *s2);
+int _strcmp(const char *s1,const char *s2);
 char *_strdup(const char *str);
 
-/************* Lis Functions **************/
+/************* List Functions **************/
 
-char *get_path(char *str, char *var);
-int main(int ac, char **av, char **env);
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloced string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: Singly linked list node structure
+ */
+typedef struct list_s
+{
+        char *str;
+        unsigned int len;
+        struct list_s *next;
+}list_t;
+
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+size_t list_len(const list_t *h);
+size_t print_list(const list_t *h);
+void free_list(list_t *head);
+char **list_to_array(list_t *head);
+
+/*************which functions **************/
 char *get_path(char *str, char *var);
 list_t *split_string(char *str, char *delim);
 char *get_full_path(char *filename, char **dir_a);
@@ -33,10 +54,7 @@ char *create_path(char *dir, char *filename);
 char *_getenv(const char *name);
 
 
-
-
-
-
+/* int main(int ac, char **av, char **env);  */
 
 
 #endif
